d166562d28cfbc429db9b7a3af5ca15e60ab676d
sunlight <sunlid@163.com>
2020-05-31 18:54:20 +0800

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by li sun on 31/5/20.
+//  Copyright © 2020 sunlight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
